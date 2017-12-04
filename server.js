9fce77baa9a099b31126551000ca8f1cab65f141
Sergey Boychuk <sergeboychuk@gmail.com>
2017-12-04 16:59:02 -0500

    pulled other users server.js to test

--- server.js
+++ server.js
@@ -2,63 +2,64 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-
-
+var port = process.env.PORT || 3000;
 var app = express();
 
-const port = process.env.PORT || 3000;
+hbs.registerPartials(__dirname + '/views/partials');
 
-hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-
-app.use((req,res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
-  console.log(log)
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if(err) {
-      console.log('unable to append server file');
-    }
-  })
-  next();
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}:  ${req.method} ${req.url}`
+     
+    console.log(log);
+    fs.appendFile('server.log',log + '\n',(err)=> {
+        if(err){console.log('unable to append to server.log')};
+    });
+    next();
 })
 
-// app.use((req,res,next) => {
-//   res.render('maint.hbs')
-// })
-
+/* app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+});
+ */
 app.use(express.static(__dirname + '/public'));
 
+
 hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
-})
+    return new Date().getFullYear();
+});
 
 hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
-})
-
-app.get('/about', (req,res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About page'
-  })
-})
+    return text.toUpperCase ();
+});
 
 app.get('/', (req,res) => {
-  res.render('index.hbs', {
-    pageTitle: 'home page',
-    welcomeMessage: 'Index page'
-  })
-})
+    res.render('home.hbs', {
+        pageTitle: 'Hello and welcome to Home Page 5',
+        newContent: 'This is text in the body'
+        // currentYear: new Date().getFullYear()
+     });
+
+     /*   res.send( {name: 'ripa', likes:['this','that n','those'] }) */
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+       pageTitle: 'This is the About Page 3'
+    });
+});
+
+app.get('/projects', (req,res) => {
+    res.render('projects.hbs', {
+       pageTitle: 'Projects'  });
+});
+
 
 app.get('/bad', (req,res) => {
-  res.send({
-    errorMessage: 'Error can not be found'
-  })
-})
-
-
-
-app.listen(port, function() {
-  console.log(`listening on port ${port}`);
+    res.send({errorMsg: 'unable'});
 });
+
+app.listen(port, ()=> {
+    console.log(`Server up on port: ${port}`);
